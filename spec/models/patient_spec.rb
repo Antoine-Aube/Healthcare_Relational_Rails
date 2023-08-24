a6899219857655825a1d851f7e359c870632a143
Antoine Aube <aaube3@gmail.com>
2023-08-24 11:04:44 -0600

    test: add relationships test to patients model tests

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,7 +3,12 @@ require "rails_helper"
 RSpec.describe Patient, type: :model do 
   let(:patient_1) {Patient.new}
   
+  describe "relationships" do 
+    it{should belong_to :hospital}
+  end
+  
   describe "Initialize" do
     it{expect(patient_1).to be_a Patient}
   end
+
 end
