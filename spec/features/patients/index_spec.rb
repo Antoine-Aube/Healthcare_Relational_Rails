148f5f69b74e39c364bbe52dfffe72cc80aaa56a
Antoine Aube <aaube3@gmail.com>
2023-08-27 15:48:54 -0600

    test: test for link to edit patient

--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -14,6 +14,46 @@ RSpec.describe "Patients Index" do
       expect(page).to have_content(patient.in_patient)
     end
 
+    it "has a link that goes to view that particular patient" do 
+      hospital = Hospital.create!(name: "St. Mary's", rating: 4, trauma: true, research: false)
+      patient_1 = hospital.patients.create!(name: "Jeff", age: 44, ailment: "Broken Leg",in_patient: true)
+      # patient_2 = hospital.patients.create!(name: "Erica", age: 32, ailment: "Broken Arm",in_patient: true)
+      
+      visit "/patients"
+      click_link "Go to Patient"
+      
+      expect(current_path).to eq("/patients/#{patient_1.id}")
+    end
     
+    it "has a link that goes to edit the patients data" do 
+      hospital = Hospital.create!(name: "St. Mary's", rating: 4, trauma: true, research: false)
+      patient_1 = hospital.patients.create!(name: "Jeff", age: 44, ailment: "Broken Leg",in_patient: true)
+
+      visit "/patients"
+      click_link "Edit Patient Data"
+
+      expect(current_path).to eq("/patients/#{patient_1.id}/edit")
+    end
+    
+    it "has a link to the hospitals index page" do 
+      visit "/hospitals"
+      
+      click_link "Hospitals"
+      expect(current_path).to eq("/hospitals")
+    end
+    
+    it "has a link to the patients index page" do 
+      visit "/hospitals"
+      
+      click_link "Patients"
+      expect(current_path).to eq("/patients")
+    end
+    
+    it "has a link to the Home page" do 
+      visit "/hospitals"
+      click_link "Homepage"
+      
+      expect(current_path).to eq("/")
+    end
   end
 end
